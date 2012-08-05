e9c64341661e08f1a736054c0e27deefabfa72eb
Matt Bauman <mbauman@gmail.com>
2012-08-05 19:45:45 -0400

    Extend EditorKit instead of DefaultEditorKit
    Use Matlab's editorKit to flush out the required abstract interface to EditorKit

--- +BetterBindings/java/BetterEditorKit.java
+++ +BetterBindings/java/BetterEditorKit.java
@@ -6,8 +6,16 @@ import javax.swing.Action;
 import javax.swing.text.TextAction;
 import java.awt.event.ActionEvent;
 
+import javax.swing.text.Caret;
+import javax.swing.text.ViewFactory;
+import javax.swing.text.Document;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
 
-public class BetterEditorKit extends DefaultEditorKit
+
+public class BetterEditorKit extends EditorKit
 /*  implements ClipboardListener */
 {
     private EditorKit baseEditorKit;
@@ -20,5 +28,43 @@ public class BetterEditorKit extends DefaultEditorKit
     public Action[] getActions()
     {
         return new Action[] {};
+    }
+
+    /* Abstract API that is imlemented by the base editorKit */
+    public Caret createCaret()
+    {
+        return baseEditorKit.createCaret();
+    }
+    public Document createDefaultDocument()
+    {
+        return baseEditorKit.createDefaultDocument();
+    }
+    public String getContentType()
+    {
+        return baseEditorKit.getContentType();
+    }
+    public ViewFactory getViewFactory()
+    {
+        return baseEditorKit.getViewFactory();
+    }
+    public void read(InputStream in, Document doc, int pos)
+        throws java.io.IOException, javax.swing.text.BadLocationException
+    {
+        baseEditorKit.read(in,doc,pos);
+    }
+    public void read(Reader in, Document doc, int pos)
+        throws java.io.IOException,javax.swing.text.BadLocationException
+    {
+        baseEditorKit.read(in,doc,pos);
+    }
+    public void write(OutputStream out, Document doc, int pos, int len)
+        throws java.io.IOException,javax.swing.text.BadLocationException
+    {
+        baseEditorKit.write(out,doc,pos,len);
+    }
+    public void write(Writer out, Document doc, int pos, int len)
+        throws java.io.IOException,javax.swing.text.BadLocationException
+    {
+        baseEditorKit.write(out,doc,pos,len);
     }
 }
